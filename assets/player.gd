c25b52e6605c39957a80b55f81184bb20f18fc2f
amwu5 <amwu5@s.sfusd.edu>
2023-12-19 11:16:20 -0800

    sound effects complete

--- assets/player.gd
+++ assets/player.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 @export var speed = 400
 
+@onready var b_gmusic = $"../BGmusic"
+@onready var enemy_hit = $"../EnemyHit"
+
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
@@ -19,3 +22,5 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	enemy_hit.play()
+	b_gmusic.stop()
